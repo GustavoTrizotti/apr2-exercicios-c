11fddd0c855d63f188ed346036ee39d1e832b191
gustavotrizotti <gustavotrizotti14@gmail.com>
2024-08-07 09:09:00 -0300

    ex a002

--- A002.c
+++ A002.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int x, y;
+    int x, y; 
     printf("\nDigite dois numeros inteiros: ");
     scanf("%d %d", &x, &y);
-    printf("Soma: %d", x + y);
+    printf("Soma: %d\nSubtracao: %d\nMultiplicacao: %d\n", x + y, x - y, x * y);
     return 0;
 }
